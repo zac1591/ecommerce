da264a271d2294959083ff191510301c768ed3ef
Isaac Abrego <abregoisaac01@gmail.com>
2020-09-19 12:09:39 -0600

    Finish google signin implementation

--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -19,13 +19,11 @@ class SignIn extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-
     this.setState({ email: "", password: "" });
   };
 
   handleChange = event => {
     const { value, name } = event.target;
-
     this.setState({ [name]: value });
   };
 
