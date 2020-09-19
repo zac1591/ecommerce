aff4b323030a0f3b61e71285827bb590fa6474ac
Isaac Abrego <abregoisaac01@gmail.com>
2020-09-19 15:29:05 -0600

    implemented firebase utils, included ability to store authenticated user

--- src/pages/sign-in-and-sign-out/sign-in-and-sign-out.jsx
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import SignIn from "../../Components/sign-in/sign-in.component";
+import SignUp from '../../Components/sign-up/sign-up.component';
+
 import "./sign-in-and-sign-out.styles.scss";
 
 const signInAndSignOut = () => (
-  <div className="signInAndSignOut">
+  <div className="signInAndSignUp">
     <SignIn />
+    <SignUp />
   </div>
 );
 
